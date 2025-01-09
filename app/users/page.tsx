abaf33bd65bd2d67a1b74ae9c0085fdee9a113f8
Madhumitha_G <madhumitha.g@waveaxis.com>
2025-01-09 20:30:25 +0100

    'Static and Dynamic Rendering'

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,14 +7,15 @@ interface User {
 
 const UserPage = async() => {
   //DISABLING CACHE 
-  // const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'})
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'})
 
   //KEEP DATA FRESH FOR CERTAIN PERIOD, THIS MEANS GET DATA FROM BACKEND EVERY 10 SEC
-  const res = await fetch('https://jsonplaceholder.typicode.com/users', {next : {revalidate: 10}});
+  // const res = await fetch('https://jsonplaceholder.typicode.com/users', {next : {revalidate: 10}});
   const user: User[] = await res.json()
   return (
     <>
     <h1>Users</h1>
+    <p>{new Date().toLocaleTimeString()}</p>
     <ul>{user.map(user => <li key={user.id}>{user.name}</li>)}</ul>
     </>
   )
